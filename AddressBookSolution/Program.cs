06228e3dc7b6de5dc229eaaa8d167ca8f5e99d0e
shashidharbollu <shashidharbollu77@gmail.com>
2022-04-20 19:17:45 +0530

    [shashidhar] Add: programme for adding contacts updated

---
+++ AddressBookSolution/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AddressBookSolution;
+class Program
+{
+    public static void Main(string[] args)
+
+    {
+        Contact contact = new Contact();
+        contact.LastName = Console.ReadLine();
+        contact.Address = Console.ReadLine();
+        contact.City = Console.ReadLine();
+        contact.State = Console.ReadLine();
+        contact.Email = Console.ReadLine();
+        contact.Zip = Convert.ToInt32(Console.ReadLine());
+        contact.Phone = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine(contact.FirstName + " "+contact.LastName+ " "+ contact.Address+ " "+ contact.City + " "+ contact.State + " "+ contact.Email+" "+ contact.Zip + " "+contact.Phone);
+
+    }
+}
